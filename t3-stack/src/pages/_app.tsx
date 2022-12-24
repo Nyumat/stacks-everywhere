ba5db5633972cdcafb00e69565a4548fe4ee24af
Thomas <nyumat18@gmail.com>
2022-12-24 03:18:21 -0800

    Added Page Transitions with FramerMotion

--- t3-stack/src/pages/_app.tsx
+++ t3-stack/src/pages/_app.tsx
@@ -1,19 +1,25 @@
 import { type AppType } from 'next/app';
 import { type Session } from 'next-auth';
 import { SessionProvider } from 'next-auth/react';
+// import DownUpTransition from '../components/DownUpTransition';
+import UpUpTransition from '../components/UpUpTransition';
+// import PopOutTransition from '../components/PopOutTransition';
 
 import { trpc } from '../utils/trpc';
 
 import '../styles/globals.css';
+import '../styles/transition.css';
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
+    <UpUpTransition>
+      <SessionProvider session={session}>
+        <Component {...pageProps} />
+      </SessionProvider>
+    </UpUpTransition>
   );
 };
 
