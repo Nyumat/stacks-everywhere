29947fcfd7d48a5792a0181adfed88daaf382c26
Thomas <nyumat18@gmail.com>
2022-12-23 17:48:39 -0800

    Added www consumption of rust wasm pkgs and webpack

--- wasm-rust-stack/src/lib.rs
+++ wasm-rust-stack/src/lib.rs
@@ -1,12 +1,20 @@
+extern crate cfg_if;
+extern crate wasm_bindgen;
+
 mod utils;
 
+use cfg_if::cfg_if;
 use wasm_bindgen::prelude::*;
 
-// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
-// allocator.
-#[cfg(feature = "wee_alloc")]
-#[global_allocator]
-static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+cfg_if! {
+    // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
+    // allocator.
+    if #[cfg(feature = "wee_alloc")] {
+        extern crate wee_alloc;
+        #[global_allocator]
+        static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+    }
+}
 
 #[wasm_bindgen]
 extern {
@@ -15,5 +23,5 @@ extern {
 
 #[wasm_bindgen]
 pub fn greet() {
-    alert("Hello, wasm-rust-stack!");
+    alert("Hello, Welcome to Nyumat's take on Rust-WASM stack!");
 }
