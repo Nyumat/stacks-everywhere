b8dde2f657f656c083d8affafba3e0b38928c911
Thomas <nyumat18@gmail.com>
2023-07-18 20:59:32 -0700

    Cat Gallery FTW

--- wasm-rust-stack/src/lib.rs
+++ wasm-rust-stack/src/lib.rs
@@ -7,27 +7,27 @@ use wasm_bindgen::prelude::*;
 use wasm_bindgen_futures::JsFuture;
 use web_sys::{window, Request, RequestInit, RequestMode, Response};
 
-macro_rules! fetch_internal {
+macro_rules! our_macro {
     ($url:expr) => {{
-        #[wasm_bindgen]
-        pub async fn fetch_internal(url: String) -> Result<JsValue, JsValue> {
-            let mut opts = RequestInit::new();
-            opts.method("GET");
-            opts.mode(RequestMode::Cors);
-            let request = Request::new_with_str_and_init(&url, &opts)?;
-            let window = window().unwrap();
-            let resp_value = JsFuture::from(window.fetch_with_request(&request)).await?;
-            let resp: Response = resp_value.dyn_into().unwrap();
-            let json = JsFuture::from(resp.json()?).await?;
+        let mut opts = RequestInit::new();
+        opts.method("GET");
+        opts.mode(RequestMode::Cors);
 
-            Ok(json)
-        }
-        fetch_internal($url.into()).await
+        let request = Request::new_with_str_and_init($url.as_str(), &opts)?;
+
+        let window = window().unwrap();
+        let resp_value = JsFuture::from(window.fetch_with_request(&request)).await?;
+
+        assert!(resp_value.is_instance_of::<Response>());
+        let resp: Response = resp_value.dyn_into().unwrap();
+
+        let json = JsFuture::from(resp.json()?).await?;
+        Ok(json)
     }};
 }
 
 
 #[wasm_bindgen]
 pub async fn fetch_external(url: String) -> Result<JsValue, JsValue> {
-    fetch_internal!(url)
+    our_macro!(url)
 }
